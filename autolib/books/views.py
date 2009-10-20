31a50b0202bd054351b464036ced7d8ade6639d3
Marcus Whybrow <marcus@marcuswhybrow.net>
2009-10-20 13:21:24 +0000

    Added a basic (hello_world) SOAP service to Django, including a view that generates the current WSDL document.

--- autolib/books/views.py
+++ autolib/books/views.py
@@ -5,15 +5,18 @@ from django.contrib.auth import authenticate, login
 # Model imports
 from django.contrib.auth.models import User
 from models import Book, Library, BookForm
+# SOAP imports
+from soaplib_handler import DjangoSoapApp, soapmethod, soap_types
+from soaplib.client import make_service_client
 
 def index(request):
-	return render_to_response('autolib/book_list.html', {
+	return render_to_response('books/book_list.html', {
 		'books' : Book.objects.all(),
 	})
 
 # Book related views
 def book(request, isbn):
-	return render_to_response('autolib/book_detail.html', {
+	return render_to_response('books/book_detail.html', {
 		'book' : get_object_or_404(Book, isbn=isbn),
 	})
 
@@ -23,44 +26,48 @@ def book_add(request):
 	else:
 		args = {'error_message' : 'Please log in to add a book'}
 	
-	return render_to_response('autolib/book_add.html', args)
+	return render_to_response('books/book_add.html', args)
 
 
 def book_add_handler(request):
 	try:
 		Book(isbn=request.POST['isbn'], title=request.POST['title'], author=request.POST['author'], library=Library(request.POST['library'])).save()
 	except (KeyError):
-		return render_to_response('autolib/book_add.html', {'error_message' : "That Book allready exists in your Library"})
+		return render_to_response('books/book_add.html', {'error_message' : "That Book allready exists in your Library"})
 	else:
 		return HttpResponseRedirect(reverse('autolib.books.views.book', args=(request.POST['isbn'],)))
 	
-
-def login(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(username=username, password=password)
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-			# Redirect here to success page
-			profile(request)
-		else:
-			login(request)
-			# Account has been disabled
-	else:
-		login(request)
-		# Incorrect login details
-
+	
 def profile(request):
-	return render_to_response('autolib/profile.html', {
+	return render_to_response('books/profile.html', {
 		'user' : request.user,
 		'libraries' : Library.objects.filter(owner=request.user)
 	})
 
 # Library related views
 def library_list(request):
-	return render_to_response('autolib/library_list.html', {
+	return render_to_response('books/library_list.html', {
 		'libraries' : Library.objects.all(),
 		'books' : Book.objects.all(),
 		'users' : User.objects.all(),
 	})
+
+
+# SOAP view
+class HelloWorldService(DjangoSoapApp):
+
+	__tns__ = 'http://192.168.1.145:8000/'
+
+	@soapmethod(soap_types.String, soap_types.Integer, _returns=soap_types.Array(soap_types.String))
+	def say_hello(self, name, times):
+		results = []
+		for i in range(0, times):
+			results.append('Hello, %s'%name)
+		return results
+
+hello_world_service = HelloWorldService()
+
+def wsdl_doc(request):
+	client = make_service_client('http://192.168.1.145:8000/hello_world/', HelloWorldService())
+	wsdl = client.server.wsdl('')
+	return HttpResponse(wsdl, mimetype='text/xml')
--- autolib/urls.py
+++ autolib/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
+	# Enable media from the same website
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
 	# Enable the Books Application URL paths
 	(r'^books/', include("autolib.books.urls")),
@@ -16,4 +20,11 @@ urlpatterns = patterns('',
 	
 	# Enable the admin URL paths
 	(r'^admin/', include(admin.site.urls)),
+	
+	# SOAP test
+	(r'^hello_world/', 'autolib.books.views.hello_world_service'),
+	(r'^hello_world/service.wsdl', 'autolib.books.views.hello_world_service'),
+	
+	(r'^wsdl/', 'autolib.books.views.wsdl_doc'),
+
 )
