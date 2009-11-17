e8e3593646e2ede8ff2f582fd512fb6080b3e4d4
Marcus Whybrow <marcus@marcuswhybrow.net>
2009-11-17 01:14:28 +0000

    Correct url regex patterns for libraries and bookshelves.

--- autolib/books/urls.py
+++ autolib/books/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls.defaults import *
 
-library_patterns = patterns('autolib.books.views',
-	(r'^$', 'library_detail'),
-	(r'^(?P<bookshelf_name>(^/)+)/', 'bookshelf_detail')
-)
-
 urlpatterns = patterns('autolib.books.views',
 
 	# Enable the Books Application URL paths
 	(r'^$', 'libraries'),
-	(r'^(?P<library_name>(^/)+)/', include(library_patterns)),
+	(r'^(?P<library_name>(^/)+)/', 'library_detail'),
+	(r'^(?P<library_name>(^/)+)/(?P<bookshelf_name>(^/)+)/', 'bookshelf_detail'),
 )
